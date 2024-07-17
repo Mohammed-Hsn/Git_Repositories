009729d6938c947ab921c8077b053e15955656c6
Mohammed Mazher Ul Hasan <mohammedmazherulhassan@gmail.com>
2024-07-17 17:54:02 +0530

    commit

--- Page Replacement/LRU.c
+++ Page Replacement/LRU.c
@@ -4,6 +4,7 @@ int main() {
     int frames[3] = {-1, -1, -1}, time[3] = {0, 0, 0};
     int pages[] = {2, 3, 2, 1, 5, 2, 4, 5, 3, 2, 5, 2};
     int n = sizeof(pages) / sizeof(pages[0]), faults = 0, t = 0;
+
     printf("\nSteps taken:\n");
     for (int i = 0; i < n; i++) {
         int hit = 0, pos = 0;
@@ -17,8 +18,11 @@ int main() {
         }
 
         if (!hit) {
-            for (int j = 1; j < 3; j++)
-                if (time[j] < time[pos]) pos = j;
+            for (int j = 1; j < 3; j++) {
+                if (time[j] < time[pos]) {
+                    pos = j;
+                }
+            }
 
             frames[pos] = pages[i];
             time[pos] = ++t;
@@ -26,9 +30,16 @@ int main() {
         }
 
         printf("%d\t", pages[i]);
-        for (int j = 0; j < 3; j++)
-            frames[j] != -1 ? printf("%d ", frames[j]) : printf("- ");
-        if (!hit) printf("--> F");
+        for (int j = 0; j < 3; j++) {
+            if (frames[j] != -1) {
+                printf("%d ", frames[j]);
+            } else {
+                printf("- ");
+            }
+        }
+        if (!hit) {
+            printf("--> F");
+        }
         printf("\n");
     }
 
